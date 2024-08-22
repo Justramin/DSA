f684443a6878aa2911559645b760793a06b9d494
Justramin <143264759+Justramin@users.noreply.github.com>
2024-08-22 22:13:04 +0530

    Initial commit

---
+++ basics.js
@@ -0,0 +1,38 @@
+
+
+
+//FEBINOCCI
+
+// function feb(n){
+//     let fib = [0,1]
+//     for (let i = 2; i <n; i++) {
+//         fib[i]=fib[i-1]+fib[i-2]
+        
+//     }
+//     return fib
+// }
+
+// console.log(feb(10));
+// console.log(feb(1));
+// console.log(feb(4));
+// console.log(feb(5));
+
+
+
+
+// FECTORAL OF A NUMBER
+
+function fact(n){
+    let result=1
+    for(let i=2 ; i<=n ; i++){
+        result = result*i
+    }
+    return result
+}
+
+console.log(fact(1));
+console.log(fact(5));
+console.log(fact(10));
+console.log(fact(15));
+
+
