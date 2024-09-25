6c1a8b6c6a499ba471917638c735420c98699442
Justramin <143264759+Justramin@users.noreply.github.com>
2024-09-25 18:37:29 +0530

    week 3 start with first

--- Week 3/Heap.js
+++ Week 3/Heap.js
@@ -66,137 +66,7 @@
 
 
 
-// class MinHeap {
-//     constructor() {
-//       this.heap = [];
-//     }
-  
-//     buildHeap(array) {
-//       this.heap = array;
-//       for (let i = Math.floor(this.heap.length / 2); i >= 0; i--) {
-//         this.heapifyDown(i);
-//       }
-//     }
-  
-//     heapifyDown(index) {
-//       let smallest = index;
-//       const leftChild = 2 * index + 1;
-//       const rightChild = 2 * index + 2;
-  
-//       if (leftChild < this.heap.length && this.heap[leftChild] < this.heap[smallest]) {
-//         smallest = leftChild;
-//       }
-  
-//       if (rightChild < this.heap.length && this.heap[rightChild] < this.heap[smallest]) {
-//         smallest = rightChild;
-//       }
-  
-//       if (smallest !== index) {
-//         [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
-//         this.heapifyDown(smallest);
-//       }
-//     }
-  
-//     remove() {
-//       if (this.heap.length === 0) return null;
-//       if (this.heap.length === 1) return this.heap.pop();
-  
-//       const root = this.heap[0];
-//       this.heap[0] = this.heap.pop();
-//       this.heapifyDown(0);
-  
-//       return root;
-//     }
-//   }
-  
-//   class MaxHeap {
-//     constructor() {
-//       this.heap = [];
-//     }
-  
-//     buildHeap(array) {
-//       this.heap = array;
-//       for (let i = Math.floor(this.heap.length / 2); i >= 0; i--) {
-//         this.heapifyDown(i);
-//       }
-//     }
-  
-//     heapifyDown(index) {
-//       let largest = index;
-//       const leftChild = 2 * index + 1;
-//       const rightChild = 2 * index + 2;
-  
-//       if (leftChild < this.heap.length && this.heap[leftChild] > this.heap[largest]) {
-//         largest = leftChild;
-//       }
-  
-//       if (rightChild < this.heap.length && this.heap[rightChild] > this.heap[largest]) {
-//         largest = rightChild;
-//       }
-  
-//       if (largest !== index) {
-//         [this.heap[index], this.heap[largest]] = [this.heap[largest], this.heap[index]];
-//         this.heapifyDown(largest);
-//       }
-//     }
-  
-//     remove() {
-//       if (this.heap.length === 0) return null;
-//       if (this.heap.length === 1) return this.heap.pop();
-  
-//       const root = this.heap[0];
-//       this.heap[0] = this.heap.pop();
-//       this.heapifyDown(0);
-  
-//       return root;
-//     }
-//   }
-  
-
-
-
-
-
-
-
-//   function heapSort(array, isMinHeap = true) {
-//     const heap = isMinHeap ? new MinHeap() : new MaxHeap();
-//     heap.buildHeap(array.slice());  // Slice to avoid modifying the original array
-    
-//     const sortedArray = [];
-//     while (heap.heap.length > 0) {
-//       sortedArray.push(heap.remove());
-//     }
-    
-//     return sortedArray;
-//   }
-  
-
-
-  
-//   // Example usage:
-//   const unsortedArray = [3, 9, 2, 1, 4, 5];
-//   console.log("Heap Sort (Min Heap):", heapSort(unsortedArray));  // Output: [1, 2, 3, 4, 5, 9]
-//   console.log("Heap Sort (Max Heap):", heapSort(unsortedArray, false));  // Output: [9, 5, 4, 3, 2, 1]
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class MaxHeap {
+class MinHeap {
     constructor() {
       this.heap = [];
     }
@@ -208,26 +78,46 @@ class MaxHeap {
       }
     }
   
-    insert(value) {
-      this.heap.push(value);
-      this.heapifyUp(this.heap.length - 1);
+    heapifyDown(index) {
+      let smallest = index;
+      const leftChild = 2 * index + 1;
+      const rightChild = 2 * index + 2;
+  
+      if (leftChild < this.heap.length && this.heap[leftChild] < this.heap[smallest]) {
+        smallest = leftChild;
+      }
+  
+      if (rightChild < this.heap.length && this.heap[rightChild] < this.heap[smallest]) {
+        smallest = rightChild;
+      }
+  
+      if (smallest !== index) {
+        [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+        this.heapifyDown(smallest);
+      }
     }
   
     remove() {
       if (this.heap.length === 0) return null;
       if (this.heap.length === 1) return this.heap.pop();
+  
       const root = this.heap[0];
       this.heap[0] = this.heap.pop();
       this.heapifyDown(0);
+  
       return root;
     }
+  }
   
-    heapifyUp(index) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      while (index > 0 && this.heap[index] > this.heap[parentIndex]) {
-        [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
-        index = parentIndex;
-        parentIndex = Math.floor((index - 1) / 2);
+  class MaxHeap {
+    constructor() {
+      this.heap = [];
+    }
+  
+    buildHeap(array) {
+      this.heap = array;
+      for (let i = Math.floor(this.heap.length / 2); i >= 0; i--) {
+        this.heapifyDown(i);
       }
     }
   
@@ -239,28 +129,138 @@ class MaxHeap {
       if (leftChild < this.heap.length && this.heap[leftChild] > this.heap[largest]) {
         largest = leftChild;
       }
+  
       if (rightChild < this.heap.length && this.heap[rightChild] > this.heap[largest]) {
         largest = rightChild;
       }
+  
       if (largest !== index) {
         [this.heap[index], this.heap[largest]] = [this.heap[largest], this.heap[index]];
         this.heapifyDown(largest);
       }
     }
   
-    getMax() {
-      return this.heap.length > 0 ? this.heap[0] : null;
+    remove() {
+      if (this.heap.length === 0) return null;
+      if (this.heap.length === 1) return this.heap.pop();
+  
+      const root = this.heap[0];
+      this.heap[0] = this.heap.pop();
+      this.heapifyDown(0);
+  
+      return root;
     }
   }
   
+
+
+
+
+
+
+
+  function heapSort(array, isMinHeap = true) {
+    const heap = isMinHeap ? new MinHeap() : new MaxHeap();
+    heap.buildHeap(array.slice());  // Slice to avoid modifying the original array
+    
+    const sortedArray = [];
+    while (heap.heap.length > 0) {
+      sortedArray.push(heap.remove());
+    }
+    
+    return sortedArray;
+  }
+  
+
+
+  
   // Example usage:
-  const maxHeap = new MaxHeap();
-  maxHeap.buildHeap([3, 9, 2, 1, 4, 5]);
-  console.log(maxHeap.heap);  // Output: [9, 4, 5, 1, 3, 2]
-  maxHeap.insert(10);
-  console.log(maxHeap.heap);  // Output: [10, 9, 5, 4, 3, 2, 1]
-  console.log("Removed:", maxHeap.remove());  // Output: Removed: 10
-  console.log(maxHeap.heap);  // Output: [9, 4, 5, 1, 3, 2]
+  const unsortedArray = [3, 9, 2, 1, 4, 5];
+  console.log("Heap Sort (Min Heap):", heapSort(unsortedArray));  // Output: [1, 2, 3, 4, 5, 9]
+  console.log("Heap Sort (Max Heap):", heapSort(unsortedArray, false));  // Output: [9, 5, 4, 3, 2, 1]
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MaxHeap {
+//     constructor() {
+//       this.heap = [];
+//     }
+  
+//     buildHeap(array) {
+//       this.heap = array;
+//       for (let i = Math.floor(this.heap.length / 2); i >= 0; i--) {
+//         this.heapifyDown(i);
+//       }
+//     }
+  
+//     insert(value) {
+//       this.heap.push(value);
+//       this.heapifyUp(this.heap.length - 1);
+//     }
+  
+//     remove() {
+//       if (this.heap.length === 0) return null;
+//       if (this.heap.length === 1) return this.heap.pop();
+//       const root = this.heap[0];
+//       this.heap[0] = this.heap.pop();
+//       this.heapifyDown(0);
+//       return root;
+//     }
+  
+//     heapifyUp(index) {
+//       let parentIndex = Math.floor((index - 1) / 2);
+//       while (index > 0 && this.heap[index] > this.heap[parentIndex]) {
+//         [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+//         index = parentIndex;
+//         parentIndex = Math.floor((index - 1) / 2);
+//       }
+//     }
+  
+//     heapifyDown(index) {
+//       let largest = index;
+//       const leftChild = 2 * index + 1;
+//       const rightChild = 2 * index + 2;
+  
+//       if (leftChild < this.heap.length && this.heap[leftChild] > this.heap[largest]) {
+//         largest = leftChild;
+//       }
+//       if (rightChild < this.heap.length && this.heap[rightChild] > this.heap[largest]) {
+//         largest = rightChild;
+//       }
+//       if (largest !== index) {
+//         [this.heap[index], this.heap[largest]] = [this.heap[largest], this.heap[index]];
+//         this.heapifyDown(largest);
+//       }
+//     }
+  
+//     getMax() {
+//       return this.heap.length > 0 ? this.heap[0] : null;
+//     }
+//   }
+  
+//   // Example usage:
+//   const maxHeap = new MaxHeap();
+//   maxHeap.buildHeap([3, 9, 2, 1, 4, 5]);
+//   console.log(maxHeap.heap);  // Output: [9, 4, 5, 1, 3, 2]
+//   maxHeap.insert(10);
+//   console.log(maxHeap.heap);  // Output: [10, 9, 5, 4, 3, 2, 1]
+//   console.log("Removed:", maxHeap.remove());  // Output: Removed: 10
+//   console.log(maxHeap.heap);  // Output: [9, 4, 5, 1, 3, 2]
 
 
 
---
+++ Week 3/workout.js
@@ -0,0 +1,205 @@
+// class node{
+//     constructor(value){
+//         this.value = value
+//         this.left = null
+//         this.right = null
+//     }
+// }
+
+
+// class BinarySearchTree{
+//     constructor(){
+//         this.root = null
+//     }
+
+//     isEmpty(){
+//         return this.root===null
+//     }
+
+//     insert(value){
+//         const newNode = new node(value)
+//         if(this.root===null){
+//             this.root = newNode
+//         }else{
+//             this.insertNode(this.root,newNode)
+//         }
+//     }
+
+//     insertNode(root,newNode){
+//         if(newNode.value<root.value){
+//             if(root.left===null){
+//                 root.left = newNode
+//             }else{
+//                 this.insertNode(root.left,newNode)
+//             }
+//         }else{
+//             if(root.right===null){
+//                 root.right=newNode
+//             }else{
+//                 this.insertNode(root.right,newNode)
+//             }
+//         }
+//     }
+
+//     preOrder(node=this.root){
+//         if(node){
+//             console.log(node.value);
+//             this.preOrder(node.left)
+//             this.preOrder(node.right)
+            
+//         }
+//     }
+//     inOrder(node = this.root){
+//        if(node){
+//         this.inOrder(node.left)
+//         console.log(node.value);
+//         this.inOrder(node.right)
+//        }
+        
+//     }
+//     postOrder(node=this.root){
+//        if (node) {
+//         this.postOrder(node.left)
+//         this.postOrder(node.right)
+//         console.log(node.value);
+        
+//        }
+//     }
+//     search(root,value){
+//         if(!root){
+//             return false
+//         }else{
+//             if(root.value===value){
+//                 return true
+//             }else if (value<root.value) {
+//                 return this.search(root.left,value)
+//             }else{
+//                 return this.search(root.right,value)
+//             }
+//         }
+//     }
+
+// }
+
+
+
+// const bst = new BinarySearchTree()
+// bst.insert(10)
+// bst.insert(11)
+// bst.insert(12)
+// bst.insert(15)
+// bst.insert(17)
+// bst.insert(19)
+// bst.insert(20)
+// bst.insert(40)
+// console.log("empty : ",bst.isEmpty());
+// bst.preOrder()
+// bst.inOrder()
+// bst.postOrder()
+// console.log(bst.search(bst.root,100));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+
+    isEmpty(){
+        return this.root===null
+    }
+
+    insert(value){
+        const newNode = new node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root,newNode)
+        }
+    }
+
+    insertNode(root,newNode){
+        if(newNode.value<root.value){
+            if(root.left===null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left,newNode)
+            }
+        }else{
+            if(root.right===null){
+                root.right=newNode
+            }else{
+                this.insertNode(root.right,newNode)
+            }
+        }
+    }
+
+    preOrder(node=this.root){
+        if(node){
+            console.log(node.value);
+            this.preOrder(node.left)
+            this.preOrder(node.right)
+        }
+    }
+
+   search(root,value){
+    if(!root){
+        return false
+    }else{
+        if(root.value===value){
+            return true
+        }else if (value<root.value) {
+            return this.search(root.left,value)
+        }else{
+            return this.search(root.right,value)
+        }
+    }
+   }
+}
+
+
+const bst = new BinarySearchTree()
+bst.insert(10)
+bst.insert(13)
+bst.insert(15)
+bst.insert(19)
+bst.insert(12)
+bst.insert(17)
+bst.insert(14)
+bst.insert(1)
+
+console.log("empty : ",bst.isEmpty());
+bst.preOrder()
+console.log(bst.search(bst.root,10));
